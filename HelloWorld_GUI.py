e8311a2311166d87f9928adec8dd1726c5f2eae9
davidagler <dwa132@psu.edu>
2020-12-04 00:22:40 -0500

    X

---
+++ HelloWorld_GUI.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk() #root is blank window
+label = Label(root, text="Hello World.") #root is blank window
+label.pack() #just pack that text in there - so lazy
+root.mainloop() #display it it in an infinite loop
